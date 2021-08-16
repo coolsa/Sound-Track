46050ea72fb7e3475ed6a91ddfd0c1a40fcce03b
Cloud Chagnon <cloud.chagnon@ucalgary.ca>
2021-08-15 23:34:44 -0600

    That debug function split did not need to be there.

--- src/main/java/xyz/coolsa/sound_track/ClientPacketHandler.java
+++ src/main/java/xyz/coolsa/sound_track/ClientPacketHandler.java
@@ -48,6 +48,7 @@ public class ClientPacketHandler {
 	 * @param buf
 	 * @param responseSender
 	 */
+	@SuppressWarnings({ "unlikely-arg-type", "resource" })
 	private void jukeboxMinecartPlayUpdate(MinecraftClient client, ClientPlayNetworkHandler handler, PacketByteBuf buf,
 			PacketSender responseSender) {
 		int entityId = buf.readInt(); // read the entity id
@@ -62,54 +63,49 @@ public class ClientPacketHandler {
 			}
 			// if it is an jukeboxminecart entity, lets go ahead and get it.
 			JukeboxMinecartEntity entity = (JukeboxMinecartEntity) client.world.getEntityById(entityId);
-			this.clientJukeboxMinecartPlay(client, entity, record);
-		});
-	}
-
-	@SuppressWarnings({ "unlikely-arg-type", "resource" })
-	private void clientJukeboxMinecartPlay(MinecraftClient client, JukeboxMinecartEntity entity, ItemStack record) {
-		// get the currently playing songs on the client.
-		SoundInstance instance = this.playingSongs.get(entity);
-		// as well as the client's sound system.
-		SoundManager soundSystem = MinecraftClient.getInstance().getSoundManager();
-		if (soundSystem == null)
-			return; // return if there is no sound system
-		Text subtitle = null; // otherwise, lets go and try to play the sound and
-		// if there is currently a sound playing
-		if (instance != null && soundSystem.isPlaying(instance)) {
-			soundSystem.stop(instance); // lets stop that sound
-			this.playingSongs.remove(instance); // remove it from this class's currently playing songs
-			instance = null; // and make sure its seen as null.
-			// otherwise
-		} else if (record.getItem() instanceof MusicDiscItem) {
-			// if the record is a vanilla (or extension of) music disc,
-			MusicDiscItem recordItem = (MusicDiscItem) record.getItem(); // get the record item
-			instance = new EntityTrackingSoundInstance(recordItem.getSound(), SoundCategory.RECORDS, 4.0f, 1.0f,
-					entity); // create a new sound instance tracking the jukebox minecart
-			subtitle = recordItem.getDescription(); // and read the description of the music disc.
-			// otherwise
-		} else if (SoundTrack.phonos.isLoaded() && SoundTrack.phonos.isCustomMusicDisc(record.getItem())) {
-			// if the record is a CustomMusicDiscItem from Phonos, read the sound event.
-			SoundEvent event = Registry.SOUND_EVENT
-					.get(Identifier.tryParse(record.getOrCreateSubNbt("MusicData").getString("SoundId")));
-			if (event != null) // if the event exists, create an instance.
-				instance = new EntityTrackingSoundInstance(event, SoundCategory.RECORDS, 4.0f, 1.0f, entity);
-			try { // also lets try to read the subtitle from the item
-				subtitle = instance.getSoundSet(MinecraftClient.getInstance().getSoundManager()).getSubtitle();
-				if(subtitle == null) throw new Exception(); // fail if subtitle is null
-			} catch (Exception e) { // if it fails, lets just make it a "custom music disc"
-				subtitle = new TranslatableText("item.phonos.custom_music_disc");
+			// get the currently playing songs on the client.
+			SoundInstance instance = this.playingSongs.get(entity);
+			// as well as the client's sound system.
+			SoundManager soundSystem = MinecraftClient.getInstance().getSoundManager();
+			if (soundSystem == null)
+				return; // return if there is no sound system
+			Text subtitle = null; // otherwise, lets go and try to play the sound and
+			// if there is currently a sound playing
+			if (instance != null && soundSystem.isPlaying(instance)) {
+				soundSystem.stop(instance); // lets stop that sound
+				this.playingSongs.remove(instance); // remove it from this class's currently playing songs
+				instance = null; // and make sure its seen as null.
+				// otherwise
+			} else if (record.getItem() instanceof MusicDiscItem) {
+				// if the record is a vanilla (or extension of) music disc,
+				MusicDiscItem recordItem = (MusicDiscItem) record.getItem(); // get the record item
+				instance = new EntityTrackingSoundInstance(recordItem.getSound(), SoundCategory.RECORDS, 4.0f, 1.0f,
+						entity); // create a new sound instance tracking the jukebox minecart
+				subtitle = recordItem.getDescription(); // and read the description of the music disc.
+				// otherwise
+			} else if (SoundTrack.phonos.isLoaded() && SoundTrack.phonos.isCustomMusicDisc(record.getItem())) {
+				// if the record is a CustomMusicDiscItem from Phonos, read the sound event.
+				SoundEvent event = Registry.SOUND_EVENT
+						.get(Identifier.tryParse(record.getOrCreateSubNbt("MusicData").getString("SoundId")));
+				if (event != null) // if the event exists, create an instance.
+					instance = new EntityTrackingSoundInstance(event, SoundCategory.RECORDS, 4.0f, 1.0f, entity);
+				try { // also lets try to read the subtitle from the item
+					subtitle = instance.getSoundSet(MinecraftClient.getInstance().getSoundManager()).getSubtitle();
+					if(subtitle == null) throw new Exception(); // fail if subtitle is null
+				} catch (Exception e) { // if it fails, lets just make it a "custom music disc"
+					subtitle = new TranslatableText("item.phonos.custom_music_disc");
+				}
 			}
-		}
-		if (subtitle != null) // if there exists a subtitle, lets display the "now playing" subtitle.
-			MinecraftClient.getInstance().inGameHud
-					.setOverlayMessage(new TranslatableText("record.nowPlaying", subtitle), true);
-		// lets put the sound into the currently playing map,
-		this.playingSongs.put(entity, instance);
-		// and if that sound is not null, lets attempt to play it via the sound system.
-		if (instance != null)
-			soundSystem.play(instance);
-		// no seaking of audio needed, because OOF is it hard for ogg files in java.
+			if (subtitle != null) // if there exists a subtitle, lets display the "now playing" subtitle.
+				MinecraftClient.getInstance().inGameHud
+						.setOverlayMessage(new TranslatableText("record.nowPlaying", subtitle), true);
+			// lets put the sound into the currently playing map,
+			this.playingSongs.put(entity, instance);
+			// and if that sound is not null, lets attempt to play it via the sound system.
+			if (instance != null)
+				soundSystem.play(instance);
+			// no seaking of audio needed, because OOF is it hard for ogg files in java.
+		});
 	}
 
 	/**
